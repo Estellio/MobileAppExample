7f3686ca9faf789bc01c5953dc8d9e8c498ce651
Student <student@Mac-28315.local>
2024-03-07 12:00:44 +0200

    Created favorites screen

--- src/screens/app/Favorites/index.js
+++ src/screens/app/Favorites/index.js
@@ -1,12 +1,23 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {View, FlatList} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "../Home/styles";
+import { products } from "../../../data/products";
+import FavoriteItem from "../../../components/FavoriteItem";
+import Header from "../../../components/Header";
 
 const Favorites = () => {
+    const renderItem = ({item}) => {
+        return (
+            <FavoriteItem {...item}/>
+        )
+    }
+
     return (
         <SafeAreaView>
-            <View>
-                <Text>Favorites</Text>
+            <View style={styles.container}>
+                <Header title="Favorites"/>
+                <FlatList data={products} renderItem={renderItem} keyExtractor={(item) => String(item.id)}/>
             </View>
         </SafeAreaView>
     )
